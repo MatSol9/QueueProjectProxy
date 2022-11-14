dee8aae5c409dbbcb4f8af44a492d9024727753e
MatSol9 <mateusz.soltys@protonmail.com>
2022-11-14 19:32:12 +0100

    Finished connection to the generator

--- src/main/java/queue/controllers/QueueController.java
+++ src/main/java/queue/controllers/QueueController.java
@@ -1,6 +1,8 @@
 package queue.controllers;
 
 import model.BasicEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,11 @@ import requests.BasicDataRequest;
 
 @RestController
 public class QueueController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(QueueController.class);
     private final DataService dataService;
 
     public QueueController(DataService dataService) {
+        LOGGER.info("Setting up QueueController");
         this.dataService = dataService;
     }
 
